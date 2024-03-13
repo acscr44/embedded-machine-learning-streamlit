9b3d25cbf9e2dd8380dea95656fb0981a938b8b0
Alejandro <acastillo44@gmail.com>
2024-03-13 19:25:54 +0100

    remove unused files

--- src/app.py
+++ src/app.py
@@ -91,49 +91,6 @@ if st.button('Submit'):
     st.text(f'Your house value is ${pred}')
     # st.write('Your pet is a cat!')
 
+st.divider()
 st.write('Todos los datos sobre viviendas incluidas en el dataset')
 st.write(df)
-st.divider()
-
-# col1, col2, col3 = st.columns(3)
-
-# with col1:
-#     color = st.color_picker('Selecciona un color', '#3475B3')
-
-# with col2:
-#     nombre = st.toggle('Mostrar el nombre')
-
-# with col3:
-#     sueldo = st.toggle('Mostrar el sueldo en la barra')
-
-# with st.container():
-#     if not df.empty:
-#         sns.set_style("white")
-#         plt.figure(figsize=(10, 8))
-#         plot = sns.barplot(x="salary", y="full name", data=df, color=color)
-
-#         if nombre:
-#             # Mostrar el full name de la gráfica:
-#             plt.yticks(df.index, df['full name'], fontsize=10)
-#         else:
-#             # Ocultar el full name de la gráfica:
-#             plt.yticks([])
-
-#         if sueldo:
-#             for i in range(len(df)):
-#                 plt.text(df.salary[i], i, df.salary[i], ha='left', va='center', color='black', fontsize=10)
-#                 print(i, df.salary[i])
-
-#         # Invierte todos los resultados del eje y:
-#         plt.gca().invert_yaxis()
-
-#         # sin etiquetas
-#         plt.xlabel('')
-#         plt.ylabel('')
-#         plt.title("Salario de empleados")
-
-#         plt.grid(axis='x', linestyle='-')
-
-#         plt.xlim(0, 4500)
-#         plt.show()
-#         st.pyplot(plot.get_figure())
